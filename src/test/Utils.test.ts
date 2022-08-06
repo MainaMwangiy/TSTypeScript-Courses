a7e8042d7787132a4980e49da27f302e41307dd5
fastlinker <charlesmaina8848@gmail.com>
2022-08-06 15:32:38 +0300

    updates

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,9 @@
+import {Utils} from '../app/Utils'
+
+describe('Utils test suite', ()=>{
+    test('first test',()=>{
+        const result = Utils.toUpperCase('abc')
+        expect(result).toBe('ABC')
+    })
+})
+
